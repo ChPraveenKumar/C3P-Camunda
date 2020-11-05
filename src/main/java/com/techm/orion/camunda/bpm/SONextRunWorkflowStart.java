8da85a739736023bc4585c1d80ba9fb2a69d1bac
Ruchita Waman Salvi <rs00502123@techmahindra.com>
2020-11-05 12:33:36 +0530

    Upload New File

---
+++ src/main/java/com/techm/orion/camunda/bpm/SONextRunWorkflowStart.java
@@ -0,0 +1,53 @@
+package com.techm.orion.camunda.bpm;
+
+import org.apache.log4j.Logger;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import com.techm.orion.camunda.common.LoggerUtil;
+import com.techm.orion.camunda.service.CamundaInvokeC3PPythonRestService;
+import com.techm.orion.camunda.service.CamundaInvokeC3PRestService;
+
+public class SONextRunWorkflowStart implements JavaDelegate{
+
+	private static final Logger logger = LoggerUtil
+			.getApplicationLogger(SONextRunWorkflowStart.class);
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		String businessKey = execution.getProcessBusinessKey();
+		Object version = execution.getVariable("version");
+		String processId = execution.getProcessInstanceId();
+	
+		if(execution.getCurrentActivityId().equals("T_C3P_SONextRun_StartEvent1")){
+			logger.info("Started SO next run wf '" + businessKey +"'");
+			logger.info("Process instance id for SO next run wf is : " + processId);
+		
+		}
+		else if(execution.getCurrentActivityId().equals("T_C3P_SONextRun_Nextrun")){
+			logger.info("Inside next run '" + businessKey +"'");
+			logger.info("Process instance id for SO next run wf is : " + processId);
+			CamundaInvokeC3PPythonRestService pythonservice=new CamundaInvokeC3PPythonRestService();
+			String result=pythonservice.nextRun(businessKey, (String) version);
+			 execution.setVariable("data", result);
+			// execution.setVariable("data", "true");
+		}
+		else if(execution.getCurrentActivityId().equals("T_C3P_SONextRun_CallNotificationApi")){
+			logger.info("Inside call notification api '" + businessKey +"'");
+			logger.info("Process instance id for SO next run wf is : " + processId);
+			CamundaInvokeC3PPythonRestService pythonservice=new CamundaInvokeC3PPythonRestService();
+			String result=pythonservice.notificationAPI(businessKey, (String) version);
+			 execution.setVariable("data", result);
+			 //execution.setVariable("data", "true");
+
+		}
+		else if(execution.getCurrentActivityId().equals("T_C3P_SONextRun_EndEvent")){
+			logger.info("Ended SO next run wf '" + businessKey +"'");
+
+		}
+		
+	
+	}
+
+}
