5a576394af45f95decb74c9ecda67de2dd3342f4
Anjireddy Reddem <anjireddy.reddem@techmahindra.com>
2021-03-02 15:02:25 +0530

    Milestone service for Service now

--- src/main/java/com/techm/orion/camunda/service/CamundaInvokeC3PRestService.java
+++ src/main/java/com/techm/orion/camunda/service/CamundaInvokeC3PRestService.java
@@ -16,6 +16,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.techm.orion.camunda.common.BpmLabels;
 import com.techm.orion.camunda.common.DBUrlSingleton;
 import com.techm.orion.camunda.common.LoggerUtil;
 
@@ -46,6 +47,8 @@ public class CamundaInvokeC3PRestService {
 	private static final String PREVALIDATE_ODL = endpointUrl + "/C3P/vnfservices/prevalidateODL";
 	private static final String PERFORM_INSTANTIATION = endpointUrl
 			+ "/C3P/Instantiation/performInstantiation";
+	private static final String UPDATE_MILESTONE = endpointUrl
+			+ "/C3P/Instantiation/pushMilestoneInfo";
 
 	public String checkDeviceReachability(String businessKey, String version) {
 		logger.info("Inside checkDeviceReachability method");
@@ -200,14 +203,24 @@ public class CamundaInvokeC3PRestService {
 		}
 		return result;
 	}
+	
+	private JSONObject getJsonObject(String businessKey, String version, String requestType) {		
+		return getJsonObject(businessKey, version, requestType, null, null);
+	}
 
 	@SuppressWarnings("unchecked")
-	private JSONObject getJsonObject(String businessKey, String version, String requestType) {
+	private JSONObject getJsonObject(String businessKey, String version, String requestType, String mileStone, String mileStoneStatus) {
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("requestId", businessKey);
 		jsonObject.put("version", version);
 		if (requestType != null) {
 			jsonObject.put("requestType", requestType);
+		}
+		if (mileStone != null) {
+			jsonObject.put("mileStoneName", mileStone);
+		}
+		if (mileStoneStatus != null) {
+			jsonObject.put("mileStoneStatus", mileStoneStatus);
 		}
 		return jsonObject;
 	}
@@ -244,8 +257,61 @@ public class CamundaInvokeC3PRestService {
 			writeOutputStream(httpConnection, getJsonObject(businessKey, version, requestType));
 			outputVar = jsonOutput(httpConnection);
 		}
-		logger.info("outputVar " + outputVar);
+		logger.info("executeBpmProcess outputVar ->" + outputVar);
+		triggerExternalService(endpointUrl, businessKey, version, requestType, outputVar);
 		return outputVar;
+	}
+
+	/***
+	 * 
+	 * This method is used to call the external service to update the milestone
+	 * status for the business key and version
+	 */
+	private void triggerExternalService(String endpointUrl, String businessKey, String version, String requestType,
+			String outputVar) {
+		if ("true".equals(BpmLabels.EXTERNAL_SERVICE.getValue()) && businessKey != null && businessKey.length() > 3) {
+			String type = businessKey.substring(0, Math.min(businessKey.length(), 4));
+			logger.info("triggerExternalService -  type" + type);
+			if ("SLGM".equals(type) || "SNAI".equals(type) || "SLGT".equals(type) || "SLGF".equals(type)
+					|| "SLGB".equals(type)) {
+				HttpURLConnection httpConnection = openHttpConnection(UPDATE_MILESTONE);
+				if (httpConnection != null) {
+					String mileStoneStatus = "true".equals(outputVar) ? "Pass" :"Fail";
+					writeOutputStream(httpConnection,
+							getJsonObject(businessKey, version, requestType, mileStoneMapping(endpointUrl), mileStoneStatus));
+					logger.info("triggerExternalService - outputVar ->" + jsonOutput(httpConnection));
+				}
+			}
+		}
+	}
+
+	/** Logic to identify the milestone for external system */
+	private String mileStoneMapping(String endpointUrl) {
+		String milestone = null;
+		if (endpointUrl.equals(PERFORM_REACHABILTY_TEST)) {
+			milestone = "Device Reachable";
+		} else if (endpointUrl.equals(DELIVER_CONFIGURATION_TEST)) {
+			milestone = "Device Configuration";
+		} else if (endpointUrl.equals(NETWORK_COMMAND_TEST)) {
+			milestone = "Network Test";
+		} else if (endpointUrl.equals(HEALTHCHECK_COMMAND_TEST)) {
+			milestone = "Health Check";
+		} else if (endpointUrl.equals(OTHERSCHECK_COMMAND_TEST)) {
+			milestone = "Other Test";
+		} else if (endpointUrl.equals(NETWORK_AUDIT_COMMAND_TEST)) {
+			milestone = "Network Audit";
+		} else if (endpointUrl.equals(FINAL_REPORT_CREATION)) {
+			milestone = "Report";
+		} else if (endpointUrl.equals(HEALTH_CHECK)) {
+			milestone = "Post Upgrade Health Check";
+		} else if (endpointUrl.equals(PERFORM_PREVALIDATE_TEST)) {
+			milestone = "Pre Validation";
+		} else if (endpointUrl.equals(PREVALIDATE_ODL)) {
+			milestone = "Pre Validation ODL";
+		} else if (endpointUrl.equals(PERFORM_INSTANTIATION)) {
+			milestone = "Instantiation";
+		}
+		return milestone;
 	}
 
 }
